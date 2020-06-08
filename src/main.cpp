277b198193ef91a11573f33a4545d3b752ef6a29
itay <itayshalev2@gmail.com>
2020-06-08 19:35:27 +0300

    start

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "main.h"
+
+int main(int argc, char **argv){
+    std::cout << "travel_path: " << std::endl;
+}
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by itay on 04/04/2020.
+//
+
+#ifndef STOWAGE_MODEL_MAIN_H
+#define STOWAGE_MODEL_MAIN_H
+
+//using namespace std;
+class main {
+
+};
+
+int main(int argc, char **argv);
+#endif //STOWAGE_MODEL_MAIN_H
